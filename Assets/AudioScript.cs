7e6169116ed072ff69bfefc508fb19b09b3379b5
SaraBeeckmans <sara@sarabeeckmans.be>
2020-12-07 09:43:27 +0000

    Added sounds + delay nurse and anastisist

---
+++ Assets/AudioScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    AudioSource myAudio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAudio = GetComponent<AudioSource>();
+        myAudio.PlayDelayed(13.0f);
+        
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
