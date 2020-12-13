ba217e10d5bb045e3eaf7e6264ebc544bac5a94a
SaraBeeckmans <sara@sarabeeckmans.be>
2020-12-13 21:24:48 +0000

    Dialog via een schema afgespeeld

---
+++ Assets/DialogScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogScript : MonoBehaviour
+{
+   public AudioSource audioSource1;
+    public AudioSource audioSource2;
+
+    void Start()
+    {
+        audioSource1.PlayScheduled(AudioSettings.dspTime+10.0);
+        double clipLength = audioSource1.clip.samples / audioSource1.clip.frequency;
+        audioSource2.PlayScheduled(AudioSettings.dspTime + clipLength+13.0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
