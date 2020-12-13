fab08070b7c1046f38ad83737f1e07850a33d3ce
SaraBeeckmans <sara@sarabeeckmans.be>
2020-12-13 20:49:58 +0000

    Classical music plays after 10 sec

---
+++ Assets/BackgroundMusic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    AudioSource myAudio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAudio = GetComponent<AudioSource>();
+        myAudio.PlayDelayed(10.0f);
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
