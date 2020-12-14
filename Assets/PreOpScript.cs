a7e4393fcecd9a01974bbe48f9505e8766d54883
SaraBeeckmans <sara@sarabeeckmans.be>
2020-12-14 10:17:26 +0000

    Pre operation script done - still needs perfect timing

---
+++ Assets/PreOpScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreOpScript : MonoBehaviour
+{
+    public AudioSource audioSource1;
+    public AudioSource audioSource2;
+    public AudioSource audioSource3;
+
+    void Start()
+    {
+        audioSource1.PlayScheduled(AudioSettings.dspTime+15.0);
+        double clipLength = audioSource1.clip.samples / audioSource1.clip.frequency;
+        audioSource2.PlayScheduled(AudioSettings.dspTime + clipLength+30.0);
+        clipLength = clipLength + audioSource2.clip.samples / audioSource2.clip.frequency;
+        audioSource3.PlayScheduled(AudioSettings.dspTime + clipLength+ 60.0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public AudioSource audioSource1;
+    public AudioSource audioSource2;
+    public AudioSource audioSource3;
+
+    void Start()
+    {
+        audioSource1.PlayScheduled(AudioSettings.dspTime+20.0);
+        double clipLength = audioSource1.clip.samples / audioSource1.clip.frequency;
+        audioSource2.PlayScheduled(AudioSettings.dspTime + clipLength+40.0);
+        clipLength = clipLength + audioSource2.clip.samples / audioSource2.clip.frequency;
+        audioSource3.PlayScheduled(AudioSettings.dspTime + clipLength+ 60.0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
